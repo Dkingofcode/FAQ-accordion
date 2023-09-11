ff7a1628b9fb4c387a89f2a37fe5c3ac5c571d1a
David <doladepo128@gmail.com>
2023-09-11 22:18:16 +0100

    aligned the text with the image

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import pattern from "./assets/bg-pattern-desktop.svg";
 import illustration from "./assets/illustration-woman-online-desktop.svg";
 import box from './assets/illustration-box-desktop.svg';
+import arrowdown from './assets/icon-arrow-down.svg';
 import './App.css';
 
 
@@ -23,20 +24,36 @@ function App() {
         <div className='questions'>
         <h1>FAQ</h1>
         
+        <div>
         <h4>How many team members can i invite?</h4>
-        <p></p>
-
+        <p>You can invite as many team members as you like.</p>
+        <img src={arrowdown} alt="arrowdown icon"  />
+        </div>
+        
+        <div>
         <h4>What is the maximum file upload size?</h4>
         <p>No more than 2GB. All files in your account must fit your alotted storage space.</p>
-      
+        <img src={arrowdown} alt="arrowdown icon"  />
+        </div>
+
+        <div>
         <h4>How do i reset my password?</h4>
-        <p></p>
+        <p>You have to go to the reset password page by clicking the link reset password. then type in your new password</p>
+        <img src={arrowdown} alt="arrowdown icon"  />
+        </div> 
 
+         <div>
         <h4>Can i cancel my subscription?</h4>
-        <p></p>
+        <p>Yeas, you can cancel your subscription any time you want, all you have to do is go to the settting page and click on subscriptions, then click on cancel or end subscription.</p>
+        <img src={arrowdown} alt="arrowdown icon"  />
+        </div>  
 
+        <div> 
         <h4>Do you provide additional support?</h4>
-        <p></p>
+        <p>Yes, but it depends on the requirements involved, we can only support tech related issues.</p>
+        <img src={arrowdown} alt="arrowdown icon"  />
+        </div>
+        
         </div>
 
       </div>
