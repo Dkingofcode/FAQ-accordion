6a37269f784551ef99ccdd1c792a2766a9bc96cc
David <doladepo128@gmail.com>
2023-09-12 19:23:45 +0100

    mobile display

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,53 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 //import reactLogo from './assets/react.svg';
 //import viteLogo from '/vite.svg';
 import pattern from "./assets/bg-pattern-desktop.svg";
 import illustration from "./assets/illustration-woman-online-desktop.svg";
 import box from './assets/illustration-box-desktop.svg';
 import arrowdown from './assets/icon-arrow-down.svg';
+import patternMobile from './assets/bg-pattern-mobile.svg';
+import mobileillustration from './assets/illustration-woman-online-mobile.svg';
 import './App.css';
 
 
 
 function App() {
+  const [patterns, setPattern] = useState(pattern);
+  const [ image, setImage ] = useState(illustration);
+  const updateImages = () => {
+    if(window.innerWidth <= 1100){
+      setImage(mobileillustration);
+      setPattern(patternMobile);  
+    }else{
+       setImage(illustration);
+       setPattern(pattern);
+    }
+  };
+  
+  useEffect(() => {
+   updateImages();  // Call it once to set initial values 
+   
+   // Add event listener to detect width changes
+   window.addEventListener('resize', updateImages);
+
+   // Clean up when component unmounts
+   return () => {
+     window.removeEventListener('resize', updateImages);
+   };
+
+  }, []);
+
 
   return (
     <>
      <div className='Container'>
       <div className='Cardcontainer'>
        <div className='images'>
-       <img src={pattern} alt="faq-image" className='pattern' />
-       <img src={illustration} alt="illustration of woman online" className='illustration' />
-       
+       <img src={patterns} alt="faq-image" className='pattern' />
+       <div class="image_container">
+       <img src={image} alt="illustration of woman online" className='illustration' />
+        </div> 
+
         </div>
 
         <div className='questions'>
@@ -57,7 +86,7 @@ function App() {
         </div>
 
       </div>
-      <img src={box} alt="illustration of box" className='box' /> 
+       <img src={box} alt="illustration of box" className='box' /> 
      </div>       
       
     </>
